a011bc75716ecf8693f2e1884e5c5b207a60b515
LamarrTheHeadcrab <40360946+LamarrTheHeadcrab@users.noreply.github.com>
2018-10-25 20:35:54 +0500

    Init

---
+++ cache/cache_users_info.php
@@ -0,0 +1,14 @@
+<?php
+
+define('PUN_USERS_INFO_LOADED', 1);
+
+$stats = array (
+  'total_users' => '3',
+  'last_user' => 
+  array (
+    'id' => '4',
+    'username' => 'test2',
+  ),
+);
+
+?>
---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+
+$db_type = 'mysqli';
+$db_host = 'localhost';
+$db_name = 'flux';
+$db_username = 'root';
+$db_password = '';
+$db_prefix = '';
+$p_connect = false;
+
+$cookie_name = 'pun_cookie_856e09';
+$cookie_domain = '';
+$cookie_path = '/';
+$cookie_secure = 0;
+$cookie_seed = 'b2dae54be5c780a2';
+
+define('PUN', 1);
